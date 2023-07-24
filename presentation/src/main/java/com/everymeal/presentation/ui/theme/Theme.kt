9e3a38e302e371718267b436fcdee6aed6ffcf1e
SsongSik <sgsk88@nate.com>
2023-07-25 00:38:31 +0900

    [feature/theme] 앱 테마 공통 Paddings 추가 (#28)

--- presentation/src/main/java/com/everymeal/presentation/ui/theme/Theme.kt
+++ presentation/src/main/java/com/everymeal/presentation/ui/theme/Theme.kt
@@ -3,11 +3,14 @@ package com.everymeal.presentation.ui.theme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    background = Color.White,
+    onBackground = Color.Black,
 )
 
 @Composable
