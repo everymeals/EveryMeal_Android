a27e5273101ee39ea459471beaa057512071787b
SsongSik <sgsk88@nate.com>
2023-07-27 01:00:29 +0900

    [feature/bottom] Bottom Navigation Item Enum 생성 (#33)

---
+++ presentation/src/main/java/com/everymeal/presentation/ui/main/BottomNavigation.kt
@@ -0,0 +1,39 @@
+package com.everymeal.presentation.ui.main
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.everymeal.presentation.R
+
+enum class BottomNavigation(
+    val route: String,
+    @DrawableRes val icon: Int,
+    @StringRes val title: Int,
+) {
+    HOME(
+        route = EveryMealRoute.HOME.route,
+        icon = R.drawable.icon_store,
+        title = R.string.bottom_nav_home
+    ),
+    UNIV_FOOD(
+        route = EveryMealRoute.UNIV_FOOD.route,
+        icon = R.drawable.icon_folk,
+        title = R.string.bottom_nav_univ_food
+    ),
+    WHAT_FOOD(
+        route = EveryMealRoute.WHAT_FOOD.route,
+        icon = R.drawable.icon_chat_bubble,
+        title = R.string.bottom_nav_what_food
+    ),
+    MY_PAGE(
+        route = EveryMealRoute.MY_PAGE.route,
+        icon = R.drawable.icon_user,
+        title = R.string.bottom_nav_my_tab
+    )
+}
+
+enum class EveryMealRoute(val route: String) {
+    HOME("home"),
+    UNIV_FOOD("univ-food"),
+    WHAT_FOOD("what-food"),
+    MY_PAGE("my-page"),
+}
