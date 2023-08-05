dd73fbe2c1a12202eaa6b7f9da28b2276303f0c9
SsongSik <sgsk88@nate.com>
2023-08-06 02:12:31 +0900

    [feature/home] HomeScreen TopAppBar, Category 화면 분리 (#36)

--- presentation/src/main/java/com/everymeal/presentation/ui/home/HomeScreen.kt
+++ presentation/src/main/java/com/everymeal/presentation/ui/home/HomeScreen.kt
@@ -2,27 +2,53 @@ package com.everymeal.presentation.ui.home
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.everymeal.presentation.R
 import com.everymeal.presentation.ui.theme.EveryMeal_AndroidTheme
+import com.everymeal.presentation.ui.theme.Gray300
+import com.everymeal.presentation.ui.theme.Gray500
+import com.everymeal.presentation.ui.theme.Gray800
+import com.everymeal.presentation.ui.theme.Paddings
 
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
 
 ) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color.White),
+    ) {
+        HomeTopAppBar()
+        HomeCategoryList()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeTopAppBar() {
     TopAppBar(
         modifier = Modifier.background(color = Color.White),
         title = {
@@ -57,6 +83,11 @@ fun HomeScreen(
             containerColor = Color.White,
         ),
     )
+}
+
+@Composable
+fun HomeCategoryList() {
+
 }
 
 @Preview
