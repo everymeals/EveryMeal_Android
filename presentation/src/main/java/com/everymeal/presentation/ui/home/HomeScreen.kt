1a7cc9b67ce6ab0f31021eb504fd3c59ab94c7c2
SsongSik <sgsk88@nate.com>
2023-08-05 22:59:55 +0900

    [feature/home] HomeScreen Top App Bar 구현

--- presentation/src/main/java/com/everymeal/presentation/ui/home/HomeScreen.kt
+++ presentation/src/main/java/com/everymeal/presentation/ui/home/HomeScreen.kt
@@ -1,21 +1,68 @@
 package com.everymeal.presentation.ui.home
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.everymeal.presentation.R
+import com.everymeal.presentation.ui.theme.EveryMeal_AndroidTheme
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
 
 ) {
-    Text(
-        text = "HomeScreen",
+    TopAppBar(
+        modifier = Modifier.background(color = Color.White),
+        title = {
+            Image(
+                imageVector = ImageVector.vectorResource(R.drawable.everymeal),
+                contentDescription = stringResource(R.string.home_everymeal_logo_text),
+            )
+        },
+        actions = {
+            IconButton(
+                onClick = {
+
+                },
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.icon_search_mono),
+                    contentDescription = stringResource(R.string.home_search_logo),
+                )
+            }
+            IconButton(
+                onClick = {
+
+                },
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.icon_heart_mono),
+                    contentDescription = stringResource(R.string.home_love_logo),
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.White,
+        ),
     )
 }
 
 @Preview
 @Composable
 fun HomeScreenPreview() {
-    HomeScreen()
+    EveryMeal_AndroidTheme {
+        HomeScreen()
+    }
 }
