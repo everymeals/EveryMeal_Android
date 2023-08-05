f732f41f3da54c1e596fa1e6eb4cd81b65cf409a
SsongSik <sgsk88@nate.com>
2023-08-06 03:06:26 +0900

    [feature/home] HomeScreen 상단 카테고리 레이아웃 구현 (#36)

--- presentation/src/main/java/com/everymeal/presentation/ui/home/HomeScreen.kt
+++ presentation/src/main/java/com/everymeal/presentation/ui/home/HomeScreen.kt
@@ -2,20 +2,26 @@ package com.everymeal.presentation.ui.home
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -43,12 +49,16 @@ fun HomeScreen(
             .background(color = Color.White),
     ) {
         HomeTopAppBar()
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 20.dp),
         ) {
-            HomeCategoryList()
+            item {
+                HomeMainTopLayout()
+                Spacer(modifier = Modifier.padding(14.dp))
+                HomeCategoryList()
+            }
         }
     }
 }
@@ -93,7 +103,7 @@ fun HomeTopAppBar() {
 }
 
 @Composable
-fun HomeCategoryList() {
+fun HomeMainTopLayout() {
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -101,10 +111,10 @@ fun HomeCategoryList() {
             .padding(horizontal = Paddings.extra, vertical = 14.dp),
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        Icon(
+        // 예시 이미지
+        Image(
             imageVector = ImageVector.vectorResource(R.drawable.ic_homemenu_recommend),
             contentDescription = stringResource(R.string.home_top_category_main_image),
-            tint = Gray500
         )
         Spacer(modifier = Modifier.padding(10.dp))
         Column {
@@ -125,6 +135,70 @@ fun HomeCategoryList() {
             contentDescription = stringResource(R.string.icon_arrow_right),
             tint = Gray500
         )
+    }
+}
+
+@Composable
+fun HomeCategoryList() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 10.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        CategoryItem(
+            R.drawable.ic_homemenu_recommend,
+            R.string.home_top_category_recommend
+        ) {
+
+        }
+        CategoryItem(
+            R.drawable.ic_homemenu_bap,
+            R.string.home_top_category_rice
+        ) {
+
+        }
+        CategoryItem(
+            R.drawable.ic_homemenu_cake,
+            R.string.home_top_category_cafe
+        ) {
+
+        }
+        CategoryItem(
+            R.drawable.ic_homemenu_beer,
+            R.string.home_top_category_drink
+        ) {
+
+        }
+    }
+}
+
+@Composable
+fun CategoryItem(
+    categoryIcon: Int,
+    categoryText: Int,
+    onClick: () -> Unit,
+) {
+    Surface(
+        shape = RoundedCornerShape(12.dp),
+    ) {
+        Column(
+            modifier = Modifier
+                .clickable { onClick() }
+                .background(Color.White, RoundedCornerShape(12.dp))
+                .padding(horizontal = 17.dp, vertical = 4.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Image(
+                imageVector = ImageVector.vectorResource(categoryIcon),
+                contentDescription = stringResource(R.string.home_top_category_list),
+            )
+            Text(
+                text = stringResource(categoryText),
+                fontSize = 12.sp,
+                color = Color.Black
+            )
+        }
     }
 }
 
