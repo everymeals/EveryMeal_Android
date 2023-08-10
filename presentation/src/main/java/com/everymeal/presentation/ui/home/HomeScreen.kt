64d5f22daa2d436e7c4ef2a29840b872d6c1e6b9
SsongSik <sgsk88@nate.com>
2023-08-10 16:05:28 +0900

    [feature/home] HomeScreen 구분선, 텍스트 추가 (#36)

--- presentation/src/main/java/com/everymeal/presentation/ui/home/HomeScreen.kt
+++ presentation/src/main/java/com/everymeal/presentation/ui/home/HomeScreen.kt
@@ -3,16 +3,18 @@ package com.everymeal.presentation.ui.home
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -21,18 +23,19 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.everymeal.presentation.R
 import com.everymeal.presentation.ui.theme.EveryMeal_AndroidTheme
+import com.everymeal.presentation.ui.theme.Gray100
 import com.everymeal.presentation.ui.theme.Gray300
 import com.everymeal.presentation.ui.theme.Gray500
 import com.everymeal.presentation.ui.theme.Gray800
@@ -51,13 +54,27 @@ fun HomeScreen(
         HomeTopAppBar()
         LazyColumn(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 20.dp),
+                .fillMaxWidth(),
         ) {
             item {
                 HomeMainTopLayout()
-                Spacer(modifier = Modifier.padding(14.dp))
                 HomeCategoryList()
+                Spacer(modifier = Modifier.padding(10.dp))
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(color = Gray100)
+                        .height(12.dp)
+                        .padding(horizontal = 0.dp)
+                )
+                Spacer(modifier = Modifier.padding(16.dp))
+                Text(
+                    modifier = Modifier.padding(start = 20.dp),
+                    text = stringResource(R.string.home_top_good_restaurant),
+                    fontSize = 20.sp,
+                    color = Color.Black,
+                    fontWeight = Bold,
+                )
             }
         }
     }
@@ -107,6 +124,7 @@ fun HomeMainTopLayout() {
     Row(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(20.dp)
             .background(Gray300, RoundedCornerShape(12.dp))
             .padding(horizontal = Paddings.extra, vertical = 14.dp),
         verticalAlignment = Alignment.CenterVertically,
@@ -143,7 +161,7 @@ fun HomeCategoryList() {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 10.dp),
+            .padding(horizontal = 20.dp),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         CategoryItem(
