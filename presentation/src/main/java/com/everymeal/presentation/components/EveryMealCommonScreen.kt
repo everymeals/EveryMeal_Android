628c4d8d7cf16927a2270c4e872e0b8095c19226
sgsk88 <sgsk88@nate.com>
2023-08-18 22:06:25 +0900

    [feature/home] star 아이콘 rating 설정 (#36)

--- presentation/src/main/java/com/everymeal/presentation/components/EveryMealCommonScreen.kt
+++ presentation/src/main/java/com/everymeal/presentation/components/EveryMealCommonScreen.kt
@@ -1,25 +1,32 @@
 package com.everymeal.presentation.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.everymeal.presentation.R
 import com.everymeal.presentation.ui.home.Restaurant
 import com.everymeal.presentation.ui.theme.Gray300
 import com.everymeal.presentation.ui.theme.Gray600
-import com.everymeal.presentation.ui.theme.Paddings
+import com.everymeal.presentation.ui.theme.Gray700
 
 @Composable
 fun EveryMealRestaurantItem(
@@ -51,6 +58,22 @@ fun EveryMealRestaurantItem(
                 fontSize = 12.sp
             )
         }
+        Spacer(modifier = Modifier.padding(4.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(
+                imageVector = ImageVector.vectorResource(R.drawable.icon_star_mono),
+                contentDescription = stringResource(R.string.icon_star),
+            )
+            Text(
+                modifier = Modifier.padding(start = 2.dp),
+                text = restaurant.rating.toString(),
+                color = Gray700,
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Medium,
+            )
+        }
     }
 }
 
@@ -59,16 +82,16 @@ fun EveryMealRestaurantItem(
 fun EveryMealRestaurantItemPreview() {
     EveryMealRestaurantItem(
         Restaurant(
-        name = "슈니",
-        category = "한식",
-        image = listOf(
-            1,
-            2,
-            3,
-        ),
-        rating = 4.5f,
-        reviewCount = 100,
-        loveCount = 100,
-    )
+            name = "슈니",
+            category = "한식",
+            image = listOf(
+                1,
+                2,
+                3,
+            ),
+            rating = 4.5,
+            reviewCount = 100,
+            loveCount = 100,
+        )
     )
 }
