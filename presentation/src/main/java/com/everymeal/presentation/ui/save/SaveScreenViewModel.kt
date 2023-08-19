0ba081d98cbc7b665cf1f14681aedbe05b6d15ea
곽의진 <yuijin0909@gmail.com>
2023-08-19 22:13:22 +0900

    [feature/save_view]: Fix HiltViewModel error

--- presentation/src/main/java/com/everymeal/presentation/ui/save/SaveScreenViewModel.kt
+++ presentation/src/main/java/com/everymeal/presentation/ui/save/SaveScreenViewModel.kt
@@ -5,9 +5,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.everymeal.presentation.ui.save.chip.ChipState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class SaveScreenViewModel : ViewModel() {
+class SaveScreenViewModel @Inject constructor() : ViewModel() {
     val elements = mutableStateListOf(
         ChipState("전체", mutableStateOf(true)),
         ChipState("밥집", mutableStateOf(false)),
