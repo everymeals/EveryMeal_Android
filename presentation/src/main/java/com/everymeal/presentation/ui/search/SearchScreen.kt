fbe86124ebaa401d0957705fa1e373532637e13e
곽의진 <yuijin0909@gmail.com>
2023-08-19 18:25:25 +0900

    [feature/search_view]: Add EmptyView

--- presentation/src/main/java/com/everymeal/presentation/ui/search/SearchScreen.kt
+++ presentation/src/main/java/com/everymeal/presentation/ui/search/SearchScreen.kt
@@ -2,7 +2,11 @@ package com.everymeal.presentation.ui.search
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
@@ -13,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -25,6 +30,7 @@ import androidx.compose.ui.unit.sp
 import com.everymeal.presentation.R
 import com.everymeal.presentation.ui.theme.Gray300
 import com.everymeal.presentation.ui.theme.Gray500
+import com.everymeal.presentation.ui.theme.Gray800
 
 @Composable
 fun SearchScreen() {
@@ -35,6 +41,7 @@ fun SearchScreen() {
             )
         }
     ) { innerPadding ->
+        EmptyView()
         SearchDetail(
             modifier = Modifier.padding(innerPadding)
         )
@@ -46,6 +53,35 @@ fun SearchDetail(
     modifier: Modifier = Modifier
 ) {
 
+}
+
+@Composable
+fun EmptyView(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.icon_store),
+                contentDescription = null,
+                modifier = Modifier.size(40.dp)
+            )
+            Spacer(modifier = Modifier.heightIn(8.dp))
+            Text(
+                text = stringResource(R.string.empty_search),
+                style = TextStyle(
+                    color = Gray800,
+                    fontWeight = FontWeight(500),
+                    fontSize = 15.sp
+                )
+            )
+        }
+    }
 }
 
 @Composable
