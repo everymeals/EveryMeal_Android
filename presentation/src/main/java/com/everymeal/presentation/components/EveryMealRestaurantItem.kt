da12d1f2ff26d397406fd758182c71d8d1fcbf75
sgsk88 <sgsk88@nate.com>
2023-08-28 20:54:42 +0900

    [feature/home] PR반영 컴포넌트 분리 (#36)

--- presentation/src/main/java/com/everymeal/presentation/components/EveryMealRestaurantItem.kt
+++ presentation/src/main/java/com/everymeal/presentation/components/EveryMealRestaurantItem.kt
@@ -46,151 +46,173 @@ fun EveryMealRestaurantItem(
             .padding(horizontal = 20.dp)
             .background(color = Color.White)
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Text(
-                text = restaurant.name,
-                color = Color.Black,
-                fontSize = 17.sp,
-                fontWeight = FontWeight.SemiBold,
-            )
-            Text(
-                modifier = Modifier
-                    .padding(start = 4.dp)
-                    .clip(RoundedCornerShape(4.dp))
-                    .background(color = Gray300)
-                    .padding(vertical = 3.dp, horizontal = 6.dp),
-                text = restaurant.category,
-                color = Gray600,
-                fontSize = 12.sp
-            )
-            Spacer(modifier = Modifier.weight(1f))
-            Image(
-                modifier = Modifier
-                    .padding(start = 4.dp),
-                imageVector = ImageVector.vectorResource(R.drawable.icon_heart_mono),
-                contentDescription = stringResource(R.string.icon_star),
-            )
-        }
+        RestaurantTitle(restaurant)
         Spacer(modifier = Modifier.padding(4.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Image(
-                imageVector = ImageVector.vectorResource(R.drawable.icon_star_mono),
-                contentDescription = stringResource(R.string.icon_star),
-            )
-            Text(
-                modifier = Modifier.padding(start = 2.dp),
-                text = restaurant.rating.toString(),
-                color = Gray700,
-                fontSize = 12.sp,
-                fontWeight = FontWeight.Medium,
-            )
-            Text(
-                modifier = Modifier.padding(start = 2.dp),
-                text = "(${restaurant.reviewCount})",
-                color = Gray700,
-                fontSize = 12.sp,
-                fontWeight = FontWeight.Medium,
-            )
-            Spacer(modifier = Modifier.weight(1f))
-            Text(
-                text = "${restaurant.loveCount}",
-                color = Gray500,
-                fontSize = 12.sp,
-                fontWeight = FontWeight.Medium,
-            )
-        }
+        RestaurantInfo(restaurant)
         Spacer(modifier = Modifier.padding(4.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            when {
-                restaurant.image.size == 3 -> {
-                    restaurant.image.forEach { image ->
-                        Image(
-                            modifier = Modifier
-                                .weight(1f)
-                                .aspectRatio(1f)
-                                .clip(RoundedCornerShape(8.dp))
-                                .padding(4.dp),
-                            painter = painterResource(id = image),
-                            contentDescription = null
-                        )
-                    }
-                }
-                restaurant.image.size == 2 -> {
-                    restaurant.image.forEach { image ->
-                        Image(
-                            modifier = Modifier
-                                .weight(1f)
-                                .aspectRatio(1f)
-                                .clip(RoundedCornerShape(8.dp))
-                                .padding(4.dp),
-                            painter = painterResource(id = image),
-                            contentDescription = null
-                        )
-                    }
-                    Spacer(modifier = Modifier.weight(1f))
-                }
-                restaurant.image.size == 1 -> {
+        RestaurantImage(restaurant)
+    }
+}
+
+@Composable
+fun RestaurantTitle(restaurant: Restaurant) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = restaurant.name,
+            color = Color.Black,
+            fontSize = 17.sp,
+            fontWeight = FontWeight.SemiBold,
+        )
+        Text(
+            modifier = Modifier
+                .padding(start = 4.dp)
+                .clip(RoundedCornerShape(4.dp))
+                .background(color = Gray300)
+                .padding(vertical = 3.dp, horizontal = 6.dp),
+            text = restaurant.category,
+            color = Gray600,
+            fontSize = 12.sp
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Image(
+            modifier = Modifier
+                .padding(start = 4.dp),
+            imageVector = ImageVector.vectorResource(R.drawable.icon_heart_mono),
+            contentDescription = stringResource(R.string.icon_star),
+        )
+    }
+}
+
+@Composable
+fun RestaurantInfo(restaurant: Restaurant) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Image(
+            imageVector = ImageVector.vectorResource(R.drawable.icon_star_mono),
+            contentDescription = stringResource(R.string.icon_star),
+        )
+        Text(
+            modifier = Modifier.padding(start = 2.dp),
+            text = restaurant.rating.toString(),
+            color = Gray700,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Medium,
+        )
+        Text(
+            modifier = Modifier.padding(start = 2.dp),
+            text = "(${restaurant.reviewCount})",
+            color = Gray700,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Medium,
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Text(
+            text = "${restaurant.loveCount}",
+            color = Gray500,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Medium,
+        )
+    }
+}
+
+@Composable
+fun RestaurantImage(restaurant: Restaurant) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        when {
+            restaurant.image.size == 3 -> {
+                restaurant.image.forEach { image ->
                     Image(
                         modifier = Modifier
                             .weight(1f)
                             .aspectRatio(1f)
                             .clip(RoundedCornerShape(8.dp))
                             .padding(4.dp),
-                        painter = painterResource(id = restaurant.image[0]),
+                        painter = painterResource(id = image),
                         contentDescription = null
                     )
-                    Spacer(modifier = Modifier.weight(2f))
                 }
-                restaurant.image.size > 3 -> {
+            }
+
+            restaurant.image.size == 2 -> {
+                restaurant.image.forEach { image ->
                     Image(
                         modifier = Modifier
                             .weight(1f)
                             .aspectRatio(1f)
                             .clip(RoundedCornerShape(8.dp))
                             .padding(4.dp),
-                        painter = painterResource(restaurant.image[0]),
+                        painter = painterResource(id = image),
                         contentDescription = null
                     )
+                }
+                Spacer(modifier = Modifier.weight(1f))
+            }
+
+            restaurant.image.size == 1 -> {
+                Image(
+                    modifier = Modifier
+                        .weight(1f)
+                        .aspectRatio(1f)
+                        .clip(RoundedCornerShape(8.dp))
+                        .padding(4.dp),
+                    painter = painterResource(id = restaurant.image[0]),
+                    contentDescription = null
+                )
+                Spacer(modifier = Modifier.weight(2f))
+            }
+
+            restaurant.image.size > 3 -> {
+                Image(
+                    modifier = Modifier
+                        .weight(1f)
+                        .aspectRatio(1f)
+                        .clip(RoundedCornerShape(8.dp))
+                        .padding(4.dp),
+                    painter = painterResource(restaurant.image[0]),
+                    contentDescription = null
+                )
+                Image(
+                    modifier = Modifier
+                        .weight(1f)
+                        .aspectRatio(1f)
+                        .clip(RoundedCornerShape(8.dp))
+                        .padding(4.dp),
+                    painter = painterResource(restaurant.image[1]),
+                    contentDescription = null
+                )
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .aspectRatio(1f)
+                        .padding(4.dp)
+                ) {
                     Image(
                         modifier = Modifier
-                            .weight(1f)
                             .aspectRatio(1f)
-                            .clip(RoundedCornerShape(8.dp))
-                            .padding(4.dp),
-                        painter = painterResource(restaurant.image[1]),
+                            .fillMaxSize(),
+                        painter = painterResource(id = restaurant.image[2]),
                         contentDescription = null
                     )
                     Box(
                         modifier = Modifier
-                            .weight(1f)
-                            .aspectRatio(1f)
-                            .padding(4.dp)
+                            .matchParentSize()
+                            .clip(RoundedCornerShape(8.dp))
+                            .background(Color.Black.copy(alpha = 0.5f)),
+                        contentAlignment = Alignment.Center
                     ) {
-                        Image(
-                            modifier = Modifier
-                                .aspectRatio(1f)
-                                .fillMaxSize(),
-                            painter = painterResource(id = restaurant.image[2]),
-                            contentDescription = null
+                        Text(
+                            text = "+${restaurant.image.size - 2}",
+                            color = Color.White,
+                            fontSize = 14.sp
                         )
-                        Box(
-                            modifier = Modifier
-                                .matchParentSize()
-                                .clip(RoundedCornerShape(8.dp))
-                                .background(Color.Black.copy(alpha = 0.5f)),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(text = "+${restaurant.image.size - 2}", color = Color.White, fontSize = 14.sp)
-                        }
                     }
                 }
             }
