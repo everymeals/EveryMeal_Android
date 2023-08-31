e4ea7ab6f251b93e4096526a098ca78f78afd0ff
sgsk88 <sgsk88@nate.com>
2023-08-31 23:37:43 +0900

    [feature/bottom_sheet] 홈화면 메인 바텀다이얼로그 셋팅 (#49)

---
+++ presentation/src/main/java/com/everymeal/presentation/components/EveryMealBottomSheetDialog.kt
@@ -0,0 +1,12 @@
+package com.everymeal.presentation.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun EveryMealMainBottomSheetDialog(
+    title: String,
+    content: String,
+    onClick : () -> Unit,
+) {
+
+}
