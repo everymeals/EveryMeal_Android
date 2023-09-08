1f041b73f6ce8f84b0247b9c5d08da3a892f9595
sgsk88 <sgsk88@nate.com>
2023-09-09 01:53:28 +0900

    [feature/more_list] DetailViewModel 생성 (#51)

---
+++ presentation/src/main/java/com/everymeal/presentation/ui/detail/DetailListViewModel.kt
@@ -0,0 +1,33 @@
+package com.everymeal.presentation.ui.detail
+
+import com.everymeal.presentation.base.BaseViewModel
+import com.everymeal.presentation.ui.detail.DetailContract.DetailEvent
+import com.everymeal.presentation.ui.detail.DetailContract.DetailState
+import com.everymeal.presentation.ui.detail.DetailContract.HomeEffect
+import javax.inject.Inject
+
+class DetailListViewModel @Inject constructor(
+
+): BaseViewModel<DetailState, HomeEffect, DetailEvent>(
+    DetailState()
+) {
+
+    override fun handleEvents(event: DetailEvent) {
+        when (event) {
+            is DetailEvent.OnClickDetailListCategoryType -> {
+                updateState {
+                    copy(
+                        detailSortCategoryType = event.detailSortCategoryType
+                    )
+                }
+            }
+            is DetailEvent.BottomSheetStateChange -> {
+                updateState {
+                    copy(
+                        bottomSheetState = event.bottomSheetState
+                    )
+                }
+            }
+        }
+    }
+}
