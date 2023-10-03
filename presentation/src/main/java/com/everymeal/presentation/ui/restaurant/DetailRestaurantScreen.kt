9e9a02f718c907f49db6d7fb8ad3d9004bf80b73
sgsk88 <sgsk88@nate.com>
2023-10-03 16:33:45 +0900

    [feature/detail_restaurant] 식당상세화면 함수 분리 (#62)

--- presentation/src/main/java/com/everymeal/presentation/ui/restaurant/DetailRestaurantScreen.kt
+++ presentation/src/main/java/com/everymeal/presentation/ui/restaurant/DetailRestaurantScreen.kt
@@ -3,6 +3,7 @@ package com.everymeal.presentation.ui.restaurant
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Divider
@@ -15,11 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -29,12 +26,53 @@ import com.everymeal.presentation.ui.theme.Gray500
 import com.everymeal.presentation.ui.theme.Gray900
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun DetailRestaurantScreen(
     detailRestaurantViewModel: DetailRestaurantViewModel = hiltViewModel(),
 ) {
-    val viewState by detailRestaurantViewModel.viewState.collectAsState()
+    LazyColumn {
+        item {
+            DetailRestaurantImage()
+        }
+        item {
+            DetailRestaurantInfo()
+        }
+        item {
+            DetailRestaurantTabLayout(detailRestaurantViewModel)
+        }
+    }
+}
+
+@Composable
+fun DetailRestaurantImage(
+
+) {
+    Column {
+        Text(
+            text = "이미지",
+            style = EveryMealTypography.labelSmall,
+        )
+    }
+}
+
+@Composable
+fun DetailRestaurantInfo(
+
+) {
+    Column {
+        Text(
+            text = "정보",
+            style = EveryMealTypography.labelSmall,
+        )
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun DetailRestaurantTabLayout(
+    viewModel : DetailRestaurantViewModel
+) {
+    val viewState by viewModel.viewState.collectAsState()
 
     val pages = listOf("정보", "사진", "리뷰(0)")
 
@@ -46,46 +84,44 @@ fun DetailRestaurantScreen(
 
     LaunchedEffect(pagerState) {
         snapshotFlow { pagerState.currentPage }.collect { page ->
-            detailRestaurantViewModel.setEvent(DetailRestaurantEvent.OnTabSelectedChanged(page))
+            viewModel.setEvent(DetailRestaurantEvent.OnTabSelectedChanged(page))
         }
     }
 
-    Column {
-        TabRow(
-            selectedTabIndex = viewState.selectedTabIndex,
-            containerColor = Color.White,
-            indicator = { tabPositions ->
-                TabRowDefaults.Indicator(
-                    Modifier.tabIndicatorOffset(tabPositions[viewState.selectedTabIndex]),
-                    color = Gray900,
-                )
-            },
-            divider = { Divider(color = Gray500) },
-        ) {
-            pages.forEachIndexed { index, title ->
-                Tab(
-                    text = { Text(
-                        text = title,
-                        style = EveryMealTypography.labelSmall,
-                    ) },
-                    selected = viewState.selectedTabIndex == index,
-                    onClick = {
-                        coroutineScope.launch {
-                            pagerState.animateScrollToPage(index)
-                        }
-                    },
-                    selectedContentColor = Gray900,
-                    unselectedContentColor = Gray500,
-                )
-            }
+    TabRow(
+        selectedTabIndex = viewState.selectedTabIndex,
+        containerColor = Color.White,
+        indicator = { tabPositions ->
+            TabRowDefaults.Indicator(
+                Modifier.tabIndicatorOffset(tabPositions[viewState.selectedTabIndex]),
+                color = Gray900,
+            )
+        },
+        divider = { Divider(color = Gray500) },
+    ) {
+        pages.forEachIndexed { index, title ->
+            Tab(
+                text = { Text(
+                    text = title,
+                    style = EveryMealTypography.labelSmall,
+                ) },
+                selected = viewState.selectedTabIndex == index,
+                onClick = {
+                    coroutineScope.launch {
+                        pagerState.animateScrollToPage(index)
+                    }
+                },
+                selectedContentColor = Gray900,
+                unselectedContentColor = Gray500,
+            )
         }
+    }
 
-        HorizontalPager(state = pagerState) { page ->
-            when (page) {
-                0 -> Text("This is Page 0", modifier = Modifier.fillMaxSize())
-                1 -> Text("This is Page 1", modifier = Modifier.fillMaxSize())
-                2 -> Text("This is Page 2", modifier = Modifier.fillMaxSize())
-            }
+    HorizontalPager(state = pagerState) { page ->
+        when (page) {
+            0 -> Text("This is Page 0", modifier = Modifier.fillMaxSize())
+            1 -> Text("This is Page 1", modifier = Modifier.fillMaxSize())
+            2 -> Text("This is Page 2", modifier = Modifier.fillMaxSize())
         }
     }
 }
