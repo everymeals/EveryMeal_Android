9e37e431528e77d7c5e6c597b807cfa747033410
sgsk88 <sgsk88@nate.com>
2023-10-07 02:16:06 +0900

    [feature/detail_restaurant] 식당 상세 정보 탭 뷰 구현 (#62)

--- presentation/src/main/java/com/everymeal/presentation/ui/restaurant/DetailRestaurantScreen.kt
+++ presentation/src/main/java/com/everymeal/presentation/ui/restaurant/DetailRestaurantScreen.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,7 +15,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.windowInsetsEndWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
@@ -33,13 +30,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -51,15 +48,15 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.everymeal.presentation.R
-import com.everymeal.presentation.components.RestaurantRating
-import com.everymeal.presentation.components.RestaurantTitle
 import com.everymeal.presentation.ui.save.SaveTopBar
 import com.everymeal.presentation.ui.theme.EveryMealTypography
 import com.everymeal.presentation.ui.theme.Gray100
 import com.everymeal.presentation.ui.theme.Gray300
+import com.everymeal.presentation.ui.theme.Gray400
 import com.everymeal.presentation.ui.theme.Gray500
 import com.everymeal.presentation.ui.theme.Gray600
 import com.everymeal.presentation.ui.theme.Gray700
+import com.everymeal.presentation.ui.theme.Gray800
 import com.everymeal.presentation.ui.theme.Gray900
 import kotlinx.coroutines.launch
 
@@ -79,7 +76,7 @@ fun DetailRestaurantScreen(
                 DetailRestaurantImage()
             }
             item {
-                DetailRestaurantInfo()
+                DetailRestaurantMainInfo()
             }
             item {
                 DetailRestaurantTabLayout(detailRestaurantViewModel)
@@ -119,7 +116,7 @@ fun DetailRestaurantImage(
 }
 
 @Composable
-fun DetailRestaurantInfo(
+fun DetailRestaurantMainInfo(
 
 ) {
     Column(
@@ -281,9 +278,52 @@ fun DetailRestaurantTabLayout(
 
     HorizontalPager(state = pagerState) { page ->
         when (page) {
-            0 -> Text("This is Page 0", modifier = Modifier.fillMaxSize())
+            0 -> DetailRestaurantTabInfo(Modifier.padding(horizontal = 20.dp))
             1 -> Text("This is Page 1", modifier = Modifier.fillMaxSize())
             2 -> Text("This is Page 2", modifier = Modifier.fillMaxSize())
+        }
+    }
+}
+
+@Composable
+fun DetailRestaurantTabInfo(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier.padding(top = 20.dp),
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(
+                modifier = Modifier.size(20.dp),
+                imageVector = ImageVector.vectorResource(R.drawable.icon_pin_location_mono),
+                contentDescription = "location",
+                colorFilter = ColorFilter.tint(Gray400),
+            )
+            Text(
+                modifier = Modifier.padding(start = 4.dp),
+                text = "서울특별시 관악구 관악로 1",
+                color = Gray800,
+                fontSize = 14.sp,
+                style = EveryMealTypography.bodySmall
+            )
+        }
+        Spacer(modifier = Modifier.padding(6.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(
+                imageVector = ImageVector.vectorResource(R.drawable.icon_call_mono),
+                contentDescription = "call",
+            )
+            Text(
+                modifier = Modifier.padding(start = 4.dp),
+                text = "1533-2233",
+                color = Gray800,
+                fontSize = 14.sp,
+                style = EveryMealTypography.bodySmall
+            )
         }
     }
 }
