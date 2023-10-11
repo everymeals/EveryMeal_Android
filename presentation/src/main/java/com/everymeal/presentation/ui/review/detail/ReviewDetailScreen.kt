5b4f57c7539c4bddab7071f616d74c5f16e75b61
KwakEuiJin <yuijin0909@gmail.com>
2023-10-12 03:27:27 +0900

    [feature/review_star_view]: add Scroll

--- presentation/src/main/java/com/everymeal/presentation/ui/review/detail/ReviewDetailScreen.kt
+++ presentation/src/main/java/com/everymeal/presentation/ui/review/detail/ReviewDetailScreen.kt
@@ -1,6 +1,9 @@
 package com.everymeal.presentation.ui.review.detail
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,7 +13,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -53,8 +58,11 @@ fun ReviewDetailScreen() {
             )
         }
     ) { innerPadding ->
+        val scrollState = rememberScrollState()
         Column(
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(scrollState)
         ) {
             UserProfileAppbar(
                 userName = "햄식이",
