2188c2295748c3af5235f65c63fddeb1ddf82111
sgsk88 <sgsk88@nate.com>
2024-01-06 19:04:20 +0900

    [feature/get_restaurant] 필터 조건 컬러 적용 (#88)

--- presentation/src/main/java/com/everymeal/presentation/ui/detail/DetailListScreen.kt
+++ presentation/src/main/java/com/everymeal/presentation/ui/detail/DetailListScreen.kt
@@ -23,8 +23,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -40,6 +42,8 @@ import com.everymeal.presentation.components.EveryMealSortCategoryBottomSheetDia
 import com.everymeal.presentation.ui.save.SaveTopBar
 import com.everymeal.presentation.ui.theme.Grey2
 import com.everymeal.presentation.ui.theme.Grey7
+import com.everymeal.presentation.ui.theme.Main100
+import com.everymeal.presentation.ui.theme.SubMain100
 import com.everymeal.presentation.ui.theme.Typography
 
 @Composable
@@ -145,7 +149,8 @@ fun DetailListScreen(
                         isCategory = true,
                         onChipClicked = {
                             detailListViewModel.setEvent(DetailContract.DetailEvent.MealRatingBottomSheetStateChange(true))
-                        }
+                        },
+                        detailListViewState = detailListViewState
                     )
 //                    DetailScreenChip(
 //                        title = "TEST신고버튼",
@@ -176,13 +181,22 @@ fun DetailScreenChip(
     title: String,
     isCategory: Boolean,
     onChipClicked: () -> Unit,
+    detailListViewState: DetailContract.DetailState? = null
 ) {
+    val isRatingOrCategory = detailListViewState?.let {
+        it.rating != 0 || it.restaurantCategoryType.name != "NONE"
+    } ?: false
+
     Surface(
         modifier = Modifier.clickable(
             indication = null,
             interactionSource = remember { MutableInteractionSource() }
         ) { onChipClicked() },
-        color = Grey2,
+        color = when {
+            detailListViewState == null -> Grey2
+            isRatingOrCategory -> SubMain100
+            else -> Grey2
+        },
         shape = RoundedCornerShape(100.dp),
     ) {
         Row(
@@ -190,9 +204,14 @@ fun DetailScreenChip(
         ) {
             Text(
                 text = title,
-                color = Grey7,
+                color = when {
+                    detailListViewState == null -> Grey7
+                    isRatingOrCategory -> Main100
+                    else -> Grey7
+                },
                 style = Typography.bodySmall,
-                modifier = Modifier.padding(start = 12.dp, end = 4.dp, top = 6.dp, bottom = 6.dp)
+                modifier = Modifier.padding(start = 12.dp, end = 4.dp, top = 6.dp, bottom = 6.dp),
+                fontWeight = FontWeight.Bold
             )
             if(isCategory) {
                 Image(
@@ -200,7 +219,12 @@ fun DetailScreenChip(
                         .padding(end = 12.dp)
                         .size(12.dp),
                     imageVector = ImageVector.vectorResource(id = R.drawable.icon_arrow_bottom),
-                    contentDescription = "arrow_bottom"
+                    contentDescription = "arrow_bottom",
+                    colorFilter = when {
+                        detailListViewState == null -> ColorFilter.tint(Grey7)
+                        isRatingOrCategory -> ColorFilter.tint(Main100)
+                        else -> ColorFilter.tint(Grey7)
+                    }
                 )
             } else {
                 Image(
